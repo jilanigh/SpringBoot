753570e453a2e0e41bbcae1430c2afed5785385b
jilanigh <gharbijilnai001@gmail.com>
2024-09-28 12:34:00 +0100

    add my files

---
+++ src/main/java/glsia/location/Order.java
@@ -0,0 +1,17 @@
+package glsia.location;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Order {
+
+}
---
+++ src/main/java/glsia/location/Product.java
@@ -0,0 +1,24 @@
+package glsia.location;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int id;
+    public String name;
+    public Double price;
+    public String description;
+}
